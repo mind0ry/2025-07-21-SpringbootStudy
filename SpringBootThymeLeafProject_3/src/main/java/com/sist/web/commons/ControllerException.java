963195815410c124ace2bd7be0609e5fb1d63d3d
KIM MINSEOK <mindory144@gmail.com>
2025-12-10 15:14:35 +0900

    2025-12-10 thymeleaf 검색

--- SpringBootThymeLeafProject_3/src/main/java/com/sist/web/commons/ControllerException.java
+++ SpringBootThymeLeafProject_3/src/main/java/com/sist/web/commons/ControllerException.java
@@ -2,7 +2,20 @@ package com.sist.web.commons;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+/*
+ *    예외 / 에러
+ *           | 수정이 불가능
+ *     | 소스상의 수정이 가능
+ *     *** 예외처리 : 사전에 에러를 방지하는 프로그램
+ *                  프로그램을 정상적으로 동작할 수 있게 만든다
+ *                  => 정상 종료를 시킨다
+ *         컴파일 예외 : 사전에 검사를 하기 때문에 반드시 예외 처리한다
+ *                     java.io / java.net / java.sql ...
+ *                     => 개발자 실수
+ *         런타임 예외 : 검사를 하지 않는다 필요시에 예외 처리를 한다
+ *                     java.util / java.lang
+ *                     => 사용자 실수
+ */
 @ControllerAdvice
 public class ControllerException {
 	
@@ -14,6 +27,7 @@ public class ControllerException {
 		System.out.println("===========================");
 	}
 	
+	// 예외+에러
 	@ExceptionHandler(Throwable.class)
 	public void throwsable(Throwable ex) {
 		System.out.println("====== Throwable 발생 ======");
