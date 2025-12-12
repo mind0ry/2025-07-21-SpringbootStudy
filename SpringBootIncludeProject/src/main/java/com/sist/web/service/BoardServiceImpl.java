1acba2445f0edaf05d09be0c35f3a1c749df07b1
KIM MINSEOK <mindory144@gmail.com>
2025-12-12 17:02:33 +0900

    2025-12-12 게시판

---
+++ SpringBootIncludeProject/src/main/java/com/sist/web/service/BoardServiceImpl.java
@@ -0,0 +1,38 @@
+package com.sist.web.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sist.web.mapper.BoardMapper;
+import com.sist.web.vo.BoardVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+	private final BoardMapper mapper;
+	@Override
+	public int boardTotalPage() {
+		// TODO Auto-generated method stub
+		return mapper.boardTotalPage();
+	}
+	@Override
+	public List<BoardVO> boardListData(int start) {
+		// TODO Auto-generated method stub
+		return mapper.boardListData(start);
+	}
+	@Override
+	public BoardVO boardDetailData(int no) {
+		// TODO Auto-generated method stub
+		mapper.hitIncrement(no);
+		return mapper.boardDetailData(no);
+	}
+	@Override
+	public void boardInsert(BoardVO vo) {
+		// TODO Auto-generated method stub
+		mapper.boardInsert(vo);
+	}
+
+}
--- SpringBootIncludeProject/src/main/java/com/sist/web/service/FoodService.java
+++ SpringBootIncludeProject/src/main/java/com/sist/web/service/FoodService.java
@@ -1,6 +1,8 @@
 package com.sist.web.service;
 
 import java.util.List;
+import java.util.Map;
+
 import com.sist.web.vo.FoodVO;
 
 
@@ -9,4 +11,6 @@ public interface FoodService {
 	public List<FoodVO> foodListData(int start);
 	public int foodTotalPage();
 	public FoodVO foodDetailData(int fno);
+	public List<FoodVO> foodFindData(Map map);
+	public int foodFindTotalPage(Map map);
 }
